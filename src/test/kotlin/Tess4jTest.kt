b5e2b0ca7d22fade63131a1fa913e96832403d7c
nchernia <Nikita.Cherniakov@t-systems.ru>
2018-12-14 11:39:19 +0300

    Update test with new service

--- src/test/kotlin/Tess4jTest.kt
+++ src/test/kotlin/Tess4jTest.kt
@@ -1,29 +1,22 @@
-import net.sourceforge.tess4j.Tesseract
-import net.sourceforge.tess4j.Tesseract1
-import net.sourceforge.tess4j.TesseractException
-import net.sourceforge.tess4j.util.LoadLibs
+import org.junit.Before
 import org.junit.Test
+import tess.TesseractService
 import java.io.File
 
 const val IMAGE_ROOT_PATH = "src/test/resources/images/"
 
 class Tess4jTest {
 
+    private lateinit var tesseractService: TesseractService
+
+    @Before
+    fun init() {
+        tesseractService = TesseractService()
+    }
+
     @Test
     fun test1() {
         val imageFile = File(IMAGE_ROOT_PATH + "sales-receipt-img1.jpg")
-        val tesseractInstance = Tesseract1()
-        tesseractInstance.setLanguage("rus")
-        tesseractInstance.setDatapath(LoadLibs.extractTessResources("rus.traineddata").parent)
-
-
-        try {
-            tesseractInstance.setHocr(true)
-
-            val result = tesseractInstance.doOCR(imageFile)
-            println(result)
-        } catch (ex: TesseractException) {
-            println(ex.message)
-        }
+        tesseractService.doOcr(imageFile)
     }
 }
