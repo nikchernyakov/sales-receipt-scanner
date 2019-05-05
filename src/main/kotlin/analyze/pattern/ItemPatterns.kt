436497da03f098c924b54ad6d52102afd77740dc
nikchernyakov <nikchernyakov@yandex.ru>
2019-05-05 21:52:25 +0300

    Add ItemPattern

--- src/main/kotlin/analyze/pattern/ItemPatterns.kt
+++ src/main/kotlin/analyze/pattern/ItemPatterns.kt
@@ -4,21 +4,24 @@ import data.document.TokenType
 
 val ITEM_PATTERNS_TEMPLATES = listOf(
         // 1 line
+        listOf(
+                listOf(TokenType.NAME, TokenType.COUNT, TokenType.ADDITIONAL_PRICE, TokenType.TOTAL_PRICE)
+        ),
         // 2 lines
         listOf(
-                listOf(TokenType.WORD),
-                listOf(TokenType.PRICE, TokenType.COUNT, TokenType.PRICE)
+                listOf(TokenType.NAME),
+                listOf(TokenType.ADDITIONAL_PRICE, TokenType.COUNT, TokenType.TOTAL_PRICE)
         ),
         listOf(
-                listOf(TokenType.NUMBER, TokenType.WORD),
-                listOf(TokenType.PRICE, TokenType.COUNT, TokenType.PRICE)
+                listOf(TokenType.NUMBER, TokenType.NAME),
+                listOf(TokenType.ADDITIONAL_PRICE, TokenType.COUNT, TokenType.TOTAL_PRICE)
         ),
 
         // 3 lines
         listOf(
-                listOf(TokenType.WORD),
+                listOf(TokenType.NAME),
                 listOf(TokenType.KEY, TokenType.NUMBER),
-                listOf(TokenType.KEY, TokenType.PRICE)
+                listOf(TokenType.KEY, TokenType.TOTAL_PRICE)
         )
 )
 
