0cd5c576a8bcf9f1213fa5cb0d77977978f20337
nikchernyakov <nikchernyakov@yandex.ru>
2019-06-05 00:47:14 +0300

    Add new PriceChecker rule  - Check for equals in word  - Add not-digit prefix and suffix checking

--- src/main/kotlin/analyze/resolver/checker/PriceChecker.kt
+++ src/main/kotlin/analyze/resolver/checker/PriceChecker.kt
@@ -8,17 +8,36 @@ object PriceChecker : TypeChecker {
         if (word.content.length <= 3) return null
 
         var hasDot = false
+        var startPassed = false
+        var digitPassed = false
+        var endPassed = false
         word.content.forEachIndexed { _, element ->
-            if (element == '.') {
+            if (digitPassed && element == '.') {
                 // Check if dot already was
                 if (hasDot) return null
                 hasDot = true
             } else {
-                if (!element.isDigit()) return null
+                if (!element.isDigit()) {
+                    if (!startPassed && !digitPassed) {
+                        // If prefix is not digit
+                        startPassed = true
+                    } else if (!endPassed && digitPassed) {
+                        // If suffix is not digit
+                        endPassed = true
+                    }
+                } else {
+                    if (endPassed) {
+                        // After price come next digit
+                        return null
+                    }
+
+                    digitPassed = true
+                }
             }
         }
 
         val hasEquals = word.neighbor.hasPrevious() && word.neighbor.previous!!.content.last() == '='
+                || word.content.first() == '='
         return if (hasDot || hasEquals) TokenType.PRICE else null
     }
 }
