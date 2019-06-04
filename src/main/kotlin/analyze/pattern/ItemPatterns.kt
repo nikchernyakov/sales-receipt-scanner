b353d0150b4616e3a9b508d206aeb5427a1e2f35
nikchernyakov <nikchernyakov@yandex.ru>
2019-06-05 00:33:13 +0300

    Add new patterns

--- src/main/kotlin/analyze/pattern/ItemPatterns.kt
+++ src/main/kotlin/analyze/pattern/ItemPatterns.kt
@@ -7,14 +7,38 @@ val ITEM_PATTERNS_TEMPLATES = listOf(
         listOf(
                 listOf(TokenType.NAME, TokenType.COUNT, TokenType.ADDITIONAL_PRICE, TokenType.TOTAL_PRICE)
         ),
+        listOf(
+                listOf(TokenType.NAME, TokenType.PERCENT, TokenType.COUNT, TokenType.ADDITIONAL_PRICE, TokenType.TOTAL_PRICE)
+        ),
+
         // 2 lines
+        listOf(
+                listOf(TokenType.NAME),
+                listOf(TokenType.COUNT, TokenType.TOTAL_PRICE)
+        ),
         listOf(
                 listOf(TokenType.NAME),
                 listOf(TokenType.ADDITIONAL_PRICE, TokenType.COUNT, TokenType.TOTAL_PRICE)
         ),
         listOf(
+                listOf(TokenType.NAME),
+                listOf(TokenType.ADDITIONAL_PRICE, TokenType.ADDITIONAL_PRICE, TokenType.COUNT, TokenType.TOTAL_PRICE)
+        ),
+        listOf(
+                listOf(TokenType.NAME),
+                listOf(TokenType.COUNT, TokenType.ADDITIONAL_PRICE, TokenType.TOTAL_PRICE)
+        ),
+        listOf(
+                listOf(TokenType.NAME),
+                listOf(TokenType.ADDITIONAL_PRICE, TokenType.COUNT, TokenType.TOTAL_PRICE, TokenType.PERCENT)
+        ),
+        listOf(
                 listOf(TokenType.NUMBER, TokenType.NAME),
                 listOf(TokenType.ADDITIONAL_PRICE, TokenType.COUNT, TokenType.TOTAL_PRICE)
+        ),
+        listOf(
+                listOf(TokenType.NAME, TokenType.COUNT, TokenType.ADDITIONAL_PRICE),
+                listOf(TokenType.PERCENT, TokenType.TOTAL_PRICE)
         ),
 
         // 3 lines
@@ -22,6 +46,30 @@ val ITEM_PATTERNS_TEMPLATES = listOf(
                 listOf(TokenType.NAME),
                 listOf(TokenType.KEY, TokenType.NUMBER),
                 listOf(TokenType.KEY, TokenType.TOTAL_PRICE)
+        ),
+        listOf(
+                listOf(TokenType.NAME, TokenType.COUNT, TokenType.ADDITIONAL_PRICE),
+                listOf(TokenType.TOTAL_PRICE),
+                listOf(TokenType.PERCENT, TokenType.ADDITIONAL_PRICE)
+        ),
+        listOf(
+                listOf(TokenType.NAME),
+                listOf(TokenType.COUNT, TokenType.ADDITIONAL_PRICE),
+                listOf(TokenType.TOTAL_PRICE)
+        ),
+        listOf(
+                listOf(TokenType.NUMBER, TokenType.NAME),
+                listOf(TokenType.COUNT, TokenType.ADDITIONAL_PRICE),
+                listOf(TokenType.TOTAL_PRICE)
+        ),
+
+        // 5 lines
+        listOf(
+                listOf(TokenType.WORD),
+                listOf(TokenType.NUMBER, TokenType.NAME),
+                listOf(TokenType.WORD),
+                listOf(TokenType.COUNT, TokenType.ADDITIONAL_PRICE),
+                listOf(TokenType.TOTAL_PRICE)
         )
 )
 
